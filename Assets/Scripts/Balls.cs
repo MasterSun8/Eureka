ca7f3f03e56555ea2c3866f6c2be48cdeea8fc5c
MasterSun8 <kacpi.doro@gmail.com>
2023-05-10 11:38:07 +0200

    10.05 school

--- Assets/Scripts/Balls.cs
+++ Assets/Scripts/Balls.cs
@@ -5,26 +5,46 @@ using UnityEngine;
 public class Balls : MonoBehaviour
 {
     public int size;
-    public GameObject[] capsules;
+    public GameObject[] capsuls;
     private int rand;
     private string str = "RGB";
+    private int y = 0;
+    private List<Tubes> capsules = new List<Tubes>();
+    private bool repeat = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(capsules == null){
-            capsules = GameObject.FindGameObjectsWithTag("Capsule");
-        }
-        int y = 0;
-        foreach(GameObject x in capsules){
-            x.GetComponent<Tubes>().id = y;
+        capsuls = GameObject.FindGameObjectsWithTag("Capsule");
+
+        foreach(GameObject x in capsuls){
+            Tubes tb = x.GetComponent<Tubes>();
+            if(tb.id == -1){
+                tb.id = y;
+            }
+            Debug.Log(tb.id);
+            capsules.Add(tb);
             y++;
-            Debug.Log(x.GetComponent<Tubes>().id);
         }
 
-        /*
-        rand = Random.Range(0, 3);
-        Debug.Log(str[rand]);*/
+        for(int i = 0; i<=capsules.Count; i++){
+            foreach(System.Char col in str){
+                do{
+                    rand = Random.Range(0, capsules.Count);
+                    Tubes temp = capsules[rand];
+                    if(temp.content.Length>3){
+                        repeat=true;
+                        continue;
+                    }
+                    temp.content = System.String.Concat(col, temp.content);
+                    repeat=false;
+                }while(repeat);
+            }
+        }
+
+        foreach (Tubes tube in capsules){
+            Debug.Log(tube.content);
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Tubes.cs
+++ Assets/Scripts/Tubes.cs
@@ -6,18 +6,15 @@ public class Tubes : MonoBehaviour
 {
     public int id = -1;
     public string content;
-    private string str = "RGB";
 
     void Start(){
-    /*
-        if(id == -1){
+        /*if(id == -1){
             try{
                 id = (int)System.Char.GetNumericValue(name[9]);
             }
             catch (System.IndexOutOfRangeException ex){
                 id = 0;
             }
-            Debug.Log(id);
         }*/
     }
 
